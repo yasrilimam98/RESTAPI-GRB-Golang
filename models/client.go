b3fd0dac2ffae16675fba9701b8f6a650d988a46
yasrilimam98 <yasrilimamofficial@gmail.com>
2021-11-25 13:36:06 +0700

    done mclient procedure in golang

--- models/client.go
+++ models/client.go
@@ -4,25 +4,30 @@ import (
 	"github.com/yasrilimam98/grb-restapi/database"
 )
 
+type Client struct {
+	Regno         int
+	Serial        string
+	Status        string
+	SisaHari      string
+	DBname        string
+	DBport        string
+	LocalNetwork  string
+	LocalPort     string
+	PublicNetwork string
+	PublicPort    string
+	Nama          string
+}
+
 func FetchAllClient(regno int, serial string) ([]Client, error) {
 	con := database.CreateCondb2()
-	rows, err := con.Query("SELECT CASE WHEN DATEDIFF(duedate,curdate()) < 0 THEN 0 ELSE 1 END , DATEDIFF(duedate,curdate()),dbname,dbport,localnetwork,localport,publicnetwork,publicport,name FROM mclient Where regno = ? AND serial = ?", regno, serial)
-
+	rows, err := con.Query("SELECT CASE WHEN DATEDIFF(duedate,curdate()) < 0 THEN 0 ELSE 1 END , DATEDIFF(duedate,curdate()),dbname,dbport,localnetwork,localport,publicnetwork,publicport,`name` FROM mclient Where regno = ? AND serial = ?", regno, serial)
+	defer rows.Close()
 	if err != nil {
 		return nil, err
 	} else {
 		clients := []Client{}
 		for rows.Next() {
-			// var regno int
-			// var serial string
-			// var duedate string
-			// var name string
-			// var dbname string
-			// var dbport string
-			// var localnetwork string
-			// var localport string
-			// var publicnetwork string
-			// var publicport string
+			var name1 string
 			var status1 string
 			var sisa1 string
 			var dbname1 string
@@ -31,12 +36,11 @@ func FetchAllClient(regno int, serial string) ([]Client, error) {
 			var locport1 string
 			var pubnet1 string
 			var pubport1 string
-			var name1 string
-			err2 := rows.Scan(&status1, &sisa1, &dbname1, &dbport1, &locnet1, &locport1, &pubnet1, &pubport1, &name1)
+			err2 := rows.Scan(&name1, &status1, &sisa1, &dbname1, &dbport1, &locnet1, &locport1, &pubnet1, &pubport1)
 			if err2 != nil {
 				return nil, err2
 			} else {
-				client := Client{regno, serial, status1, sisa1, dbname1, dbport1, locnet1, locport1, pubnet1, pubport1}
+				client := Client{regno, serial, name1, status1, sisa1, dbname1, dbport1, locnet1, locport1, pubnet1, pubport1}
 				clients = append(clients, client)
 			}
 		}
--- models/client_entity.go
+++ models/client_entity.go
@@ -1,14 +1,14 @@
 package models
 
-type Client struct {
-	Regno         int    
-	Serial        string
-	Status          string 
-	Name          string
-	DBname        string
-	DBport        string
-	LocalNetwork  string
-	LocalPort     string
-	PublicNetwork string 
-	PublicPort    string 
-}
+// type Client struct {
+// 	Regno         int    
+// 	Serial        string
+// 	Status          string 
+// 	Name          string
+// 	DBname        string
+// 	DBport        string
+// 	LocalNetwork  string
+// 	LocalPort     string
+// 	PublicNetwork string 
+// 	PublicPort    string 
+// }
